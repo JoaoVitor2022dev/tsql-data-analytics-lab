8d59cc9894c8f2322e231d4ea74d082119369609
JoaoVitor2022dev <githubconta2021@gmail.com>
2026-06-02 11:07:52 -0300

    Description: 11. JSON + SQL

--- 11. JSON + SQL/main.sql
+++ 11. JSON + SQL/main.sql
@@ -1,33 +1,53 @@
 CREATE PROCEDURE import_json_transacoess
+AS
+BEGIN
 
-AS 
-   BEGIN 
+    SET NOCOUNT ON;
 
-   SET NOCOUNT ON; -- Quando você quer ver quantas linhas cada comando afetou 
+    DECLARE @json NVARCHAR(MAX);
 
-   DECLARE @json NVARCHAR(MAX)
-
-   -- ler o arquivo onde está o arquivo JSON 
-   SELECT @json = BulkColumn
-   FROM OPENROWSET (
-        BULK 'C:\Users\jennifer\OneDrive\Desktop\JV\Estudos\SQL\Base_de_dados\exemplo_tabela.json', 
+    SELECT @json = BulkColumn
+    FROM OPENROWSET (
+        BULK 'C:\Users\jennifer\OneDrive\Desktop\JV\Estudos\SQL\Base_de_dados\exemplo_tabela.json',
         SINGLE_CLOB
-    ) AS [JsonData]
+    ) AS JsonData;
 
--- Criar a tabela com condição se existir, deletar a tabela e criar novamente
-IF OBJECT_ID('dbo.tb.Transacoes') IS NOT NULL 
-BEGIN 
-    CREATE TABLE dbo.tb.Transacoes (
-        DataTransacao:    VARCHAR(30),
-        Cliente:          VARCHAR(100),
-        TipoTransacao:    VARCHAR(50),
-        Valor_Transacoes: NUMERIC(10,2),  
-        Bandeira:         VARCHAR(50),
-        Aprovado:         VARCHAR(10)
-    ) 
-END 
+    IF OBJECT_ID('dbo.tb_Transacoes') IS NOT NULL
+        DROP TABLE dbo.tb_Transacoes;
 
- -- Inserir os dados do json na tabela 
+    CREATE TABLE dbo.tb_Transacoes (
+        DataTransacao VARCHAR(30),
+        Cliente VARCHAR(100),
+        TipoTransacao VARCHAR(50),
+        Valor_Transacoes NUMERIC(10,2),
+        Bandeira VARCHAR(50),
+        Aprovado VARCHAR(10)
+    );
 
+    INSERT INTO dbo.tb_Transacoes (
+        DataTransacao,
+        Cliente,
+        TipoTransacao,
+        Valor_Transacoes,
+        Bandeira,
+        Aprovado
+    )
+    SELECT
+        DataTransacao,
+        Cliente,
+        TipoTransacao,
+        Valor_Transacoes,
+        Bandeira,
+        Aprovado
+    FROM OPENJSON(@json)
+    WITH (
+        DataTransacao VARCHAR(30),
+        Cliente VARCHAR(100),
+        TipoTransacao VARCHAR(50),
+        Valor_Transacoes NUMERIC(10,2),
+        Bandeira VARCHAR(50),
+        Aprovado VARCHAR(10)
+    );
 
-
+END;
+GO
